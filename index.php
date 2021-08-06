721982bf83b2adfdf281aeaea3bdf77ac3c2f8a1
Vanshaj-kaushal <vanshajkaushal1.0@gmail.com>
2021-08-06 15:26:14 +0530

    changed ui of signup, added scroll content for homepage

--- index.php
+++ index.php
@@ -1,58 +1,193 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>ELECTRONIX</title>
-        <link rel="stylesheet" href="css/master.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ELECTRONIX</title>
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
 
-    <!-- onload="looping();" -->
+</head>
 
-    <body>
+<!-- onload="looping();" -->
 
-        <!-- header file -->
-        <?php include 'inc/header.php'; ?>
+<body>
+
+    <!-- header file -->
+    <?php include 'inc/header.php'; ?>
 
 
-        <br>
-        <div class="products">
-            <div class="slideshow">
-                <div class="imgCont">
-                    <img src="img/1.png" alt="" id="img1">
-                    <img src="img/2.jpg" alt="" id="img2">
-                    <img src="img/3.jpg" alt="" id="img3">
-                    <img src="img/4.jpg" alt="" id="img4">
-                    <img src="img/5.jpg" alt="" id="img5">
-                    <img src="img/6.gif" alt="" id="img6">
-                    <img src="img/7.jpg" alt="" id="img7">
-                    <img src="img/8.jpg" alt="" id="img8">
-                    <img src="img/9.jpg" alt="" id="img9">
-                    <!-- don't remove last image, else the 9th image is put out of loop -->
-                    <img src="img/img.jpg" alt="" id="extra">
-                </div>
+    <br>
+    <div>
+        <div class="slideshow">
+            <div class="imgCont">
+                <img src="img/1.png" alt="" id="img1">
+                <img src="img/2.jpg" alt="" id="img2">
+                <img src="img/3.jpg" alt="" id="img3">
+                <img src="img/4.jpg" alt="" id="img4">
+                <img src="img/5.jpg" alt="" id="img5">
+                <img src="img/6.gif" alt="" id="img6">
+                <img src="img/7.jpg" alt="" id="img7">
+                <img src="img/8.jpg" alt="" id="img8">
+                <img src="img/9.jpg" alt="" id="img9">
+                <!-- don't remove last image, else the 9th image is put out of loop -->
+                <img src="img/img.jpg" alt="" id="extra">
             </div>
-
-            <div id="theme">
-                <button onclick="changeTheme();">Theme</button>
-            </div>
-
         </div>
-        <br>
+
+        <!-- <div id="theme">
+                <button onclick="changeTheme();">Theme</button>
+            </div> -->
+
+    </div>
+    <br>
+
+
+    <section id="main">
+        <div>
+
+            <section>
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
+
+            </section>
+        
+            <section>
+                
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
+                
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+            
+            </section>
+            
+            
+            <section>
+                
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+                
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>    
+
+            </section>
 
 
 
-        <!-- footer file -->
-        <?php include'inc/footer.php'; ?>
+            <section>
+                
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
 
-    </body>
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
 
-    <script>
+            </section>
+
+
+
+            <section>
+
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+                
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
+
+            </section>
+
+
+            <section>
+                
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
+
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+
+            </section>
+
+
+
+            <section>
+                
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
+
+            </section>
+
+
+
+            <section>
+
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
+
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+
+            </section>
+
+
+            <section>
+                <div>
+                    <img class="sImg" src="img/tv.jpg" alt="">
+                </div>
+
+                <div class="content">
+                    <h2>See Big Think Big</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem deserunt, iusto eaque atque maiores nobis porro debitis, optio dolores ea voluptatum nemo consectetur dolorem similique.</p>
+                </div>
+
+            </section>
+        
+        </div>
+
+    </section>
+
+
+
+    <!-- footer file -->
+    <?php include 'inc/footer.php'; ?>
+
+</body>
+
+<!-- <script>
 
         function changeTheme(){
             console.log("called");
@@ -62,7 +197,8 @@
             }
         }
 
-    </script>
+    </script> -->
 
-    <script src="js/slideshow.js" charset="utf-8"></script>
+<script src="js/slideshow.js" charset="utf-8"></script>
+
 </html>
--- signup.php
+++ signup.php
@@ -1,65 +1,146 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-        <link rel="stylesheet" href="css/signup.css">
-    </head>
-    <body>
-        
-        <?php
 
-            if($_POST["given"] == 1)
-            {
-                echo '<p style="background: lightgreen; color: white;"> Success! your email ';
-                echo $_POST["email"];
-                echo ' has been registered with us.<br> Please continue shopping with us!!!</p>';
-            }
-        ?>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/signup.css">
+</head>
+
+<body>
+
+    <!-- header file here -->
+
+    <?php include 'inc/header.php'; ?>
 
 
-        <!-- header file here -->
 
-        <?php include'inc/header.php'; ?>
+    <?php
+
+    if ($_POST["given"] == 1) {
+        echo '<p style="background: lightgreen; color: white;"> Success! your email ';
+        echo $_POST["email"];
+        echo ' has been registered with us.<br> Please continue shopping with us!!!</p>';
+    }
+    ?>
 
 
-        <form class="" action="" method="post">
-            <fieldset>
-                <div class="inputContainer">
-                    <div class="fNameContainer">
-                        <input type="text" name="fName" placeholder="Enter first name: ">
+
+    <form class="" action="" method="post">
+        <fieldset id="main">
+            <div class="inputContainer">
+                <div>
+                    <div class="nameContainer">
+                        <fieldset>
+
+                            <input type="text" name="name" class="form_input" placeholder="" value="" required>
+                            <label for="" class="form_label">Enter your full Name </label>
+
+                        </fieldset>
                     </div>
-                    <div class="lNameContainer">
-                        <input type="text" name="lName" placeholder="Enter last name: ">
-                    </div>
-                    <br>
-                    <div class="sexInputContainer">
-                        Sex:
-                        <input type="radio" name="sex" value="male">Male
-                        <input type="radio" name="sex" value="female">Female
-                    </div>
+
 
                     <!-- con: contact -->
-                    <div class="conContainer">
-                        <input type="number" name="conNum" placeholder="Enter mobile number: ">
-                        <input type="email" name="email" placeholder="Enter email: ">
+                    <div class="number">
+                        <fieldset>
+
+                            <input type="text" name="telPhone" class="form_input" placeholder="" value="" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" maxlength="10" required>
+                            <label for="" class="form_label">Enter mobile number </label>
+
+                        </fieldset>
+                    </div>
+
+
+                    <div class="email">
+                        <fieldset>
+                        
+                            <input type="email" name="email" class="form_input" placeholder="" required>
+                            <label for="" class="form_label">Enter email </label>
+
+                        </fieldset>
+
+                    </div>
+
+
+
+                    <div class="gender">
+                        
+                        <input type="radio" id="gender" class="form_input" name="gender" value="male" required>Male
+                        <input type="radio" id="gender" class="form_input" name="gender" value="female" required>Female
+                        <input type="radio" id="gender" class="form_input" name="gender" value="female" required>Prefer not say
+                    </div>
+
+                </div>
+
+
+                <div>
+
+                    <div>
+                        <fieldset>
+
+                            <input type="text" name="username" class="form_input" placeholder="" value="" required>
+                            <label for="" class="form_label">Create a username </label>
+                        </fieldset>
+                    </div>
+
+                    <div>
+                        <fieldset>
+                            
+                            <input type="password" name="pass" class="form_input" placeholder="" value="" required>
+                            <label for="" class="form_label">Enter a password </label>
+
+                        </fieldset>
+                    </div>
+
+
+                    <div>
+                        <fieldset>
+
+                            <input type="password" name="pass" class="form_input" placeholder="" value="">
+                            <label for="" class="form_label">Re-enter Password: </label>
+
+                        </fieldset>
                     </div>
                 </div>
 
-                <br>
-                <div class="passContainer">
-                    <input type="password" name="pass" placeholder="Enter a password: ">
-                    <br>
-                    <input type="password" name="pass" placeholder="Re-enter password: ">
-                </div>
-                <br>
-                <br>
-                <div class="submitContainer">
-                    <input type="hidden" name="given" value="1">
-                    <input type="submit" name="submit" value="Sign Up">
-                </div>
-            </fieldset>
-        </form>
+            </div>
 
-    </body>
+
+
+
+
+            <div class="submit">
+                <input type="hidden" name="given" value="1">
+                <input type="submit" id="submit" name="submit" value="Sign Up">
+            </div>
+        </fieldset>
+    </form>
+
+</body>
+
+
+<script>
+    // const x = document.querySelectorAll("input");
+    // const y = document.querySelectorAll("legend");
+
+    // for(let i=0;i<x;i++)
+    // {
+    //     let str;
+    //     x[i].addEventListener("focus", function() {
+    //         str = this.placeholder;
+    //         console.log("called");
+    //         x[i].placeholder = "";
+    //         y[i].style.display = "inline";
+    //     });
+
+    //     x[i].addEventListener("blur", function() {
+    //         console.log("called_2");
+    //         x[i].placeholder = str;
+    //         y[i].style.display = "none";
+    //     });
+    // }
+
+</script>
+
+
 </html>
